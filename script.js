678289b01a856ae469625e49ccc57a6322691a81
Gokul222002 <ngokulakrishnan22082002@gmail.com>
2024-11-21 16:36:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Change background image on hover
+document.body.addEventListener("mouseover", () => {
+    document.body.style.backgroundImage = "url('../image/sad-boy-anime-cb3fxaudx0fi8yju.jpg')";
+});
+
+document.body.addEventListener("mouseout", () => {
+    document.body.style.backgroundImage = "url('../image/wallpaperflare.com_wallpaper.jpg')";
+});
+
