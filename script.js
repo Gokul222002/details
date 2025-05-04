fcac1de96fe9c89c1941e86ad145e5b1a5239961
Gokul222002 <ngokulakrishnan22082002@gmail.com>
2025-05-04 20:34:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.body.addEventListener("mouseover", () => {
-    document.body.style.backgroundImage = "url('image/sad-boy-anime-cb3fxaudx0fi8yju.jpg')";
+    document.body.style.backgroundImage = "url('https://www.google.com/url?sa=i&url=https%3A%2F%2Falphacoders.com%2Fanime-4k-wallpapers&psig=AOvVaw1lijGaSkL_25Vkk4LhVkZ3&ust=1746457406518000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKCVk4CLio0DFQAAAAAdAAAAABAE')";
 });
 
 document.body.addEventListener("mouseout", () => {
