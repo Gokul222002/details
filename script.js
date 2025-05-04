df3ff5dc100cef64970f172268efd821d1b57a8c
Gokul222002 <ngokulakrishnan22082002@gmail.com>
2025-05-04 20:39:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.body.addEventListener("mouseover", () => {
-    document.body.style.backgroundImage = "url('https://www.google.com/url?sa=i&url=https%3A%2F%2Falphacoders.com%2Fanime-4k-wallpapers&psig=AOvVaw1lijGaSkL_25Vkk4LhVkZ3&ust=1746457406518000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKCVk4CLio0DFQAAAAAdAAAAABAE')";
+    document.body.style.backgroundImage = "url('https://images.alphacoders.com/133/1336046.jpg')";
 });
 
 document.body.addEventListener("mouseout", () => {
-    document.body.style.backgroundImage = "url('image/wallpaperflare.com_wallpaper.jpg')";
+    document.body.style.backgroundImage = "url('https://images.alphacoders.com/132/1322181.jpg')";
 });
