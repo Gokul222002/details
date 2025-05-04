cc9536691d242bd572da2f852cbb17931b338974
Gokul222002 <ngokulakrishnan22082002@gmail.com>
2025-05-04 20:31:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Change background image on hover
 document.body.addEventListener("mouseover", () => {
-    document.body.style.backgroundImage = "url('../image/sad-boy-anime-cb3fxaudx0fi8yju.jpg')";
+    document.body.style.backgroundImage = "url('image/sad-boy-anime-cb3fxaudx0fi8yju.jpg')";
 });
 
 document.body.addEventListener("mouseout", () => {
-    document.body.style.backgroundImage = "url('../image/wallpaperflare.com_wallpaper.jpg')";
+    document.body.style.backgroundImage = "url('image/wallpaperflare.com_wallpaper.jpg')";
 });
-
